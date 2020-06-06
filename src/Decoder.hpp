590ebab715d6e15d8e8dced1e3121a829739805c
Jim Lundgren <jim_lundgren@live.com>
2020-06-06 22:06:56 +0100

    update ignore list + comment

--- src/Decoder.hpp
+++ src/Decoder.hpp
@@ -1,8 +1,7 @@
 #include "BaseCoder.hpp"
 
 namespace lzw {
-// TODO: Need to take in a file path to decode, and
-// create an encoded file who's name is based on it.
+
 class Decoder : public BaseCoder {
 public:
     int run(std::string inFile) override;
