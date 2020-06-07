4505aec648efe1742297cdcd1064257b73a630e7
Jim Lundgren <jim_lundgren@live.com>
2020-06-07 14:47:40 +0100

    add test

--- Makefile
+++ Makefile
@@ -3,5 +3,11 @@ all: main
 main: src/main.cpp
 	cl /EHsc /std:c++17 src/main.cpp src/BaseCoder.cpp src/Decoder.cpp src/Encoder.cpp src/TextChecker.cpp
 
+test: test/test.cpp
+	cl /EHsc /I src /std:c++17 test\test.cpp src\BaseCoder.cpp src\Decoder.cpp src\Encoder.cpp src\TextChecker.cpp
+
 clean:
 	del main.exe main.obj BaseCoder.obj Decoder.obj Encoder.obj TextChecker.obj
+
+cleanTest:
+	del test.exe test.obj BaseCoder.obj Decoder.obj Encoder.obj TextChecker.obj
---
+++ test/test.cpp
@@ -0,0 +1,85 @@
+#include "Decoder.hpp"
+#include "Encoder.hpp"
+#include "TextChecker.hpp"
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+
+namespace fs = std::filesystem;
+
+// TODO: Use GoogleTest framework for testing.
+// TODO: Add more test cases for edge cases and
+// checking all code paths (negative tests).
+
+namespace {
+class TestDirRAII {
+    fs::path _testDir;
+    bool _testPassed = false;
+
+public:
+    TestDirRAII() : _testDir("TestDir")
+    {
+        fs::create_directory(_testDir);
+    }
+
+    ~TestDirRAII()
+    {
+        if (_testPassed) {
+            fs::remove_all("TestDir");
+        } else {
+            std::cout << "Preserving test files upon failure." << std::endl;
+        }
+    }
+
+    std::string string() {
+        return _testDir.string();
+    }
+
+    void setTestPassed(bool result) {
+        _testPassed = result;
+    }
+};
+
+} //namespace
+
+int main()
+{
+    TestDirRAII testDir;
+
+    std::string origFile(testDir.string() + "\\OriginalText");
+    std::ofstream origFileOfs(origFile);
+    if (!origFileOfs.is_open()) {
+        throw std::runtime_error(("Unable to open file: " + origFile));
+    }
+
+    origFileOfs << "Hello World!" << std::endl;
+    origFileOfs << "aAaAaAaAaA" << std::endl;
+    origFileOfs << "dnsfiohgu4b7547hfndy875@)*$&^40303jfidsn" << std::endl;
+    origFileOfs << "burija" << std::endl;
+
+    origFileOfs.close();
+
+    std::string encFile(testDir.string() + "\\EncodedText.ENC");
+    lzw::Encoder enc;
+    enc.run(origFile, encFile);
+
+    std::string decFile(testDir.string() + "\\DecodedText");
+    lzw::Decoder dec;
+    dec.run(encFile, decFile);
+
+    lzw::TextChecker tc(origFile, decFile);
+    auto const isSame(tc.checkSame());
+
+    if (isSame) {
+        std::cout << "Test Passed" << std::endl;
+        testDir.setTestPassed(true);
+    } else {
+        std::cout << "Test Failed" << std::endl;
+        testDir.setTestPassed(false);
+        return -1;
+    }
+
+    return 0;
+}
